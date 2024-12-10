04703cd42bfad9f9c5cbc62d9db880c01db40f26
Zach Dodson <zdodson21@gmail.com>
2024-12-10 14:07:58 -0500

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './styles/App.css'
 import LinkBox from './components/LinkBox.jsx'
 
 function App() {
   return (
     <>
-      <div className="site-wrapper">
-        <h1>My Links</h1>
+      <div className="content-wrapper">
+        <div className="profile-info">
+          <img id="profile-pic" src="https://avatars.githubusercontent.com/u/91283128?v=4" alt="Zach Dodson Profile Picture" />
+          <p>Frontend Web Developer</p>
+          <h1>My Links</h1>
+        </div>
         <div className="links">
           <LinkBox 
             URL="https://github.com/zdodson21"
--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -1,13 +1,17 @@
 import '../styles/LinkBox.css'
+import githubDarkLogo from '../assets/GitHub_Invertocat_Dark.svg'
 
 function LinkBox(props) {
   return (
     <>
-      <div className="link-box-wrapper">
         {/* TODO need image for site + alt text */}
         {/* TODO need anchor URL + text variable*/}
-        <a href={props.URL}>{props.linkText}</a>
-      </div>
+        <a href={props.URL} className='link-wrapper' target='_blank'>
+          <img src={githubDarkLogo} alt="GitHub Logo" />
+          <div className="text-container">
+            <p>{props.linkText}</p>
+          </div>
+        </a>
     </>
   )
 }
