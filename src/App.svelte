38ffab3a322cd4c41214f07dc302022909197363
Zach Dodson <zdodson21@gmail.com>
2025-06-16 09:49:15 -0400

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import LinkBox from './lib/LinkBox.svelte';
+</script>
+
+<main>
+  <LinkBox alt="test" text="text">
+    Hello
+  </LinkBox>
+</main>
---
+++ src/lib/LinkBox.svelte
@@ -0,0 +1,17 @@
+<script>
+  let props = $props();
+</script>
+
+<div class="link-box">
+  <img src="{props.src}" alt="{props.alt}">
+  <p>
+    {props.text}
+  </p>
+</div>
+
+<style>
+  .link-box {
+    background-color: white;
+    color: black;
+  }
+</style>
